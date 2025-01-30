9a7fbdd9b13b650d7ab8d9e69c280d14468a822a
ElNotsniw <winstonjosh010604@gmail.com>
2025-01-29 20:27:45 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+// Task 1 - Product Price Management
+
+let prices = [34, 76, 58, 22, 91]         // Original Output
+prices.unshift (30)                       // Adds 30 to the beginning of the array
+prices.shift()                            // Deletes the beginning value
+console.log("Updated Prices:", prices)    // Updated Prices
